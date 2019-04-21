147111a053f04d1a8afcc1195c284d74fd5f6682
azizburibaev <azizburibaev@yahoo.com>
2019-04-20 19:57:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    var myMovie = new Movie();
+    var listOfMovies = [];
+
+    $("#evalRating").click( function() {
+        
+         myMovie = new Movie($("#movieName").val(), parseInt( $("#movieRating").val()));
+
+        if ( !myMovie.isValid() ) {
+                $("#showSuccessEntry").text("Your need to enter 1,2,3,4, or 5 to rate the movie"); 
+                $("#movieName").val( "" );
+                $("#movieRating").val( "" );
+                $("#movieName").focus();
+            
+        } else {
+                    $("#showSuccessEntry").text('Valid entry');
+                 //alert("good "+"job");
+                  $("#movieName").val( "" );
+                  $("#movieRating").val( "" );
+                  $("#movieName").focus();
+                  listOfMovies.push(myMovie);
+        for (let index = 0; index < listOfMovies.length; index++) {
+                const element = listOfMovies[index];
+                 $("#justText").text(element.concat());  
+            
+        }
+        
+          
+        }
+        
+    });
+    
+    $("#listMovies").click( function(){
+     
+        for (let index = 0; index < listOfMovies.length; index++) {
+            const element = listOfMovies[index];
+            var para = document.createElement("h4");
+            var node = document.createTextNode(element.concat());
+            para.appendChild(node);
+            var divelement = document.getElementById("forOutput");
+            divelement.appendChild(para);
+           
+        }
+    }
+    );
+   
+});
---
+++ movie.js
@@ -0,0 +1,21 @@
+
+var Movie = function(aName, aRating) { //declaring constructor function with parametrs
+	this.name = aName;
+	this.rating = aRating;
+    this.isValid = function() {
+            if (isNaN(this.rating) ) { //checking if valid numbers are entered
+                return false;
+            } else if ( this.rating < 1 || this.rating > 5 ) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        this.concat = function() {
+            var x;
+            x = this.name +" "+ this.rating;
+            return x.toString();
+            
+        }
+            
+    };
